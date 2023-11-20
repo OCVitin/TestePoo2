0849d10d37309af6d209bbd55e331fa6568b46f4
Vito <103535367+OCVitin@users.noreply.github.com>
2023-11-20 08:37:12 -0300

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import usuarioRoutes from './routes/usuarioRoute';
+import leilaoRoutes from './routes/leilaoRoute';
+import lanceRoutes from './routes/lanceRoute';
+
+const app   = express();
+const port  = 3000;
+
+app.use(express.json());
+
+app.use('/', usuarioRoutes);
+app.use('/', lanceRoutes);
+app.use('/', leilaoRoutes);
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Servidor está ouvindo na porta ${port}`);
+});
